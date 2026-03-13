461282248eb343554a95e56a953b95fe744b1929
V1INTIATIVE <publix.v1@yahoo.com>
2026-03-13 22:18:56 +0530

    vex fix

---
+++ v.js
@@ -0,0 +1,29 @@
+const dropDown = document.getElementById('class');
+const dropDown_subject = document.getElementById('subject');
+const dropDown_study = document.getElementById('study');
+
+
+
+
+let choice = "";
+let choice_subject = "";
+let choice_study = "";
+
+dropDown.addEventListener('change', (event) => {
+    choice = event.target.value;
+    console.log("Choice updated to: " + choice);
+});
+
+dropDown_subject.addEventListener('change', (event) => {
+    choice_subject = event.target.value;
+    console.log("Subject updated to: " + choice_subject);
+});
+
+dropDown_study.addEventListener('change', (event) => {
+    choice_study = event.target.value;
+    console.log("Study updated to: " + choice_study);
+});
+
+
+
+alert("Test ");
